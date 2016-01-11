4faffbf710e15b295daf8312a84f0e3a7c33bc52
Younes Serraj <younes.serraj@gmail.com>
2016-01-11 12:00:59 +0100

    Added hsn_node_alloc()

---
+++ src/hsn_node_alloc.c
@@ -0,0 +1,35 @@
+/*
+** This file is part of HSN (Human-Scale Network)
+**
+** HSN: secure, distributed, SPOF-free network of trust
+** Copyright (C) 2016 Younes SERRAJ
+**
+** This program is free software; you can redistribute it and/or
+** modify it under the terms of the GNU General Public License
+** version 2 as published by the Free Software Foundation.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+** or see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include "hsn_node.h"
+#include "peer.h"
+#include "xfunctions.h"
+
+t_hsn_node		*hsn_node_alloc()
+{
+  t_hsn_node		*hsn_node;
+
+  hsn_node = xmalloc(sizeof(t_hsn_node));
+  if (hsn_node)
+    hsn_node_init(hsn_node);
+  return (hsn_node);
+}
